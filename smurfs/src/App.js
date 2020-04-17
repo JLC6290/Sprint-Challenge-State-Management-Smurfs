a1ced0e6298826463d08d1311a5e7579401273f6
James <jlc6290@gmail.com>
2020-04-17 10:32:53 -0500

    initial commit

---
+++ smurfs/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "./App.css";
+
+export default function App() {
+    
+    return (
+      <div className="App">
+        <h1>SMURFS! 2.0 W/ <strike>Redux</strike> Context!</h1>
+        
+      </div>
+    );
+}
